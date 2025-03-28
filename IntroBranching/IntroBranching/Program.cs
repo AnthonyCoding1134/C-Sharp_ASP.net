c6f36661931acead9b500110853acf3a8b71f5af
AnthonyCoding1134 <155090896+AnthonyCoding1134@users.noreply.github.com>
2025-03-28 10:46:49 -0400

    intro branching

---
+++ IntroBranching/IntroBranching/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroBranching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myFaveHobby = "reading"; // Change this to your favorite hobby   
+
+            if (myFaveHobby == "sports")
+            {
+                Console.WriteLine("That's a great way to stay active and have fun!");
+            }
+            else if (myFaveHobby == "video games")
+            {
+                Console.WriteLine("Video games can be a fun way to relax and unwind.");
+            }
+            else if (myFaveHobby == "reading")
+            {
+                Console.WriteLine("Reading is a wonderful way to explore new worlds and ideas.");
+            }
+            else
+            {
+                Console.WriteLine("So what do you do?");
+            }
+
+
+
+        }
+    }
+}
